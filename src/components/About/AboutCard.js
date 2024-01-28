42406cf23ef719c329b978a98444457192e16362
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-01-28 12:54:02 +0530

    made changes in footer, about, techstack and home

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,25 +8,21 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Sukrut Rahane </span>
-            from <span className="purple"> Nashik, India.</span>
-            <br /> I am a Third Year student pursuing B.E.
-            in Artificial intelligence & Data Science from Pune University.
+            Hi Everyone, I am <span className="purple">Ishika Singh </span>
+            from <span className="purple"> Agra, India.</span>
+            <br /> I am a Third Year student pursuing degree in Electrical Engineering with a specialization in Computer Science.
             <br />
-            Additionally, I am enthusiastic about Machine Learning.
+            Additionally, I am enthusiastic about Web Development.
             <br />
             <br />
             Some of my hobbies are 
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Singing
             </li>
             <li className="about-activity">
-              <ImPointRight /> Partying
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> EDM
+              <ImPointRight /> Reading
             </li>
           </ul>
           
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -23,7 +22,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/sukrutrahane"
+                href="https://github.com/ishikasingh21"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,17 +32,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/sukrutrahane"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/sukrutrahane/"
+                href="https://www.linkedin.com/in/ishika-singh-a43b78232/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +42,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/sukrutrahane"
+                href="https://www.instagram.com/ishh.ikaa/?next=%2F"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
