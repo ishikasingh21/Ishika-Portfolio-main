b6b5670413346e9088dc3114729cc03d3b104c98
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-02-07 09:10:03 +0530

    image needed

--- src/projectData.js
+++ src/projectData.js
@@ -1,21 +1,21 @@
 const projects = [
     {
       title: 'Sales Predictor',
-      image: 'action needed !',
+      //image: 'action needed !',
       description: 'blah blah blah',
       tools: ['Python', 'Pandas', 'NumPy', 'Matplotlib', 'Scikit-Learn'],
       github: 'https://github.com/ishikasingh21/Sales-predictor',
     },
     {
       title: 'Bank Transaction Analysis',
-      image: 'action needed !',
+      //image: 'action needed !',
       description: 'blah blah blah',
       tools: ['Python', 'Microsoft Excel'],
       github: 'https://github.com/ishikasingh21/datacleaning-project-1',
     },
     {
       title: 'Plant Disease Detection ',
-      image: 'action needed !',
+      //image: 'action needed !',
       description: 'blah blah blah.',
       tools: ['Python',  'TensorFlow',  'Keras',  'Jupyter Notebook',  'Scikit-Learn'],
       github: 'https://github.com/ishikasingh21/Plant-Disease-detection',
