5782238e774ef128ca67e9d924f863c7775188d8
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-02-07 08:55:26 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Footer from "./components/Footer";
-import Contact from "./components/Contact";
+
 import {
   BrowserRouter as Router,
   Route,
@@ -37,8 +37,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/project" element={<projectData />} />
-          <Route path="/contact" element={<Contact />} />
+        
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
