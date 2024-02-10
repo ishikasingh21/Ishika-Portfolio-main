ad05381261af3aa628fba097027b962f2a4b1675
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-02-10 22:04:26 +0530

    colors

--- src/components/experiences.js
+++ src/components/experiences.js
@@ -1,20 +1,20 @@
 // experiences.js
 const experiences = [
     {
-      title: 'Front End Web Developer Intern',
-      company: 'Edunet Foundation',
+      title: <span className="purple">Front End Web Developer Intern</span>,
+      company: <span className="purple">Edunet Foundation</span>,
       date: 'June 2023 - July 2023',
       description: 'As a Front-End Developer intern at Edunet Foundation, I designed intuitive interfaces for educational platforms, integrating innovative features to enhance user experience. I sharpened my coding skills and gained valuable insights into the intersection of technology and education.',
     },
     {
-      title: 'Data Analyst Intern',
-      company: 'Edulyt India',
+      title: <span className="purple">Data Analyst Intern</span>,
+      company: <span className="purple">Edulyt India</span>,
       date: 'May 2023 - June 2023',
       description: 'As a Data Analyst Intern at Edulyt India, I delved into intricate datasets, uncovering insights to enhance educational strategies.',
     },
     {
-      title: 'Internship Trainee',
-      company: 'Dakshinanchal Vidyut Vitran Nigam Limited',
+      title: <span className="purple">Internship Trainee</span>,
+      company: <span className="purple">Dakshinanchal Vidyut Vitran Nigam Limited</span>,
       date: 'June 2022- June 2022 ',
       description: 'Interning at DVVNL in Agra, I immersed myself in data analysis and field trips, gaining insight into the intricacies of power distribution. Beyond the tasks, the camaraderie among colleagues showed me the human side of the energy industry, leaving an enduring impact on my journey.',
     },
