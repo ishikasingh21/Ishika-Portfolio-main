6401e0d483a07ad98721a226a92e7a80f44d203f
Ishika Singh <ishika003.singh@gmail.com>
2024-09-27 19:26:38 +0530

    edited pics

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/girl.png";
+import myImg from "../../Assets/pixelcut-export.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
